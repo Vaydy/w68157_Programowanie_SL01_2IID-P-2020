4d391f72ed8b89107d998424e676ddcc508434d2
Vaydy <4examccna@gmail.com>
2023-05-16 10:34:20 +0200

    Lab4

---
+++ Lab4/Zadanie2/script.js
@@ -0,0 +1,19 @@
+const student = {
+  name: "Max",
+  surname: "Throwback",
+  numer: "w68157",
+  ccna: 3,
+  wf: 2,
+  fizyka: 4,
+  numbers: [2, 3, 4],
+  sredocena: function() {
+    var sum = 0;
+    for (var i = 0; i < this.numbers.length; i++) {
+      sum += this.numbers[i];
+    }
+    var average = sum / this.numbers.length;
+    console.log("Середня оцінка: " + average);
+  }
+};
+
+student.sredocena();
