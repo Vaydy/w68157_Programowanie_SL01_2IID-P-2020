ae88b5f110791efd6bf144a8ea7035239dc1dbd8
Vaydy <4examccna@gmail.com>
2023-05-17 16:11:07 +0200

    Lab5

---
+++ Lab5/Zadanie1/test.js
@@ -0,0 +1,6 @@
+let age = prompt("Give me your age:");
+if (age >= 18) {
+    window.alert("Jestes pelnoletni");
+} else {
+    window.alert("Jestes nepelnoletni");
+}
--- Lab5/test.js
+++
@@ -1,6 +0,0 @@
-let age = prompt("Give me your age:");
-if (age >= 18) {
-    console.log("Jestes pelnoletni");
-} else {
-    console.log("Jestes nepelnoletni");
-}
