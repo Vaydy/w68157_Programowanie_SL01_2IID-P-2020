e053422666633ecd0cf6df8b894c8fad7eb15765
Vaydy <4examccna@gmail.com>
2023-05-18 13:58:21 +0200

    Lab6

--- Lab6/Zadanie2,3/script.js
+++ Lab6/Zadanie2,3/script.js
@@ -8,3 +8,11 @@ function changeColor(buttonId) {
         }
     }
 }
+function toggleVisibility(elementId) {
+    var element = document.getElementById(elementId);
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
